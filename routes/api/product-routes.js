bf3d58a9519d350d175d5a5be461c57f994b234c
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-09-02 16:44:30 -0400

    updated tag routes - get route to get a singe product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,23 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  ry {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'Product not found with this id' });
+      return;
+    }
+
+    res.json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
