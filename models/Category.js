236bd847fa957abfcf30b8e0377462dec8f19d55
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-09-02 18:14:41 -0400

    added code to add category id to db category associations

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,12 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -20,5 +26,10 @@ Category.init(
     modelName: 'category',
   }
 );
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = Category;
